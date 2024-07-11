c3819b06396cc5f41f3205462fc946acd4c4aba0
Marcelo Garita <mgaritaf@outlook.com>
2024-07-11 14:52:27 -0600

    fixing boxShadow issue

--- src/components/CountryCardContainer.tsx
+++ src/components/CountryCardContainer.tsx
@@ -5,7 +5,12 @@ interface Props {
 }
 const CountryCardContainer = ({ children }: Props) => {
   return (
-    <Box borderRadius={5} width={"100%"} overflow={"hidden"}>
+    <Box
+      borderRadius={5}
+      width={"100%"}
+      overflow={"hidden"}
+      boxShadow={"rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;"}
+    >
       {children}
     </Box>
   );
